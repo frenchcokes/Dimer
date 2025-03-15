e1014d4d15028d38b0cefbb7c8f6330b12496afc
Kevin Tu <tukevin274@gmail.com>
2025-03-15 12:54:18 -0600

    Change camera zoom

--- Scenes/interacting_component.gd
+++ Scenes/interacting_component.gd
@@ -9,9 +9,7 @@ func _input(event: InputEvent) -> void:
 		if current_interactions:
 			can_interact = false
 			interact_label.hide()
-			
 			await current_interactions[0].interact.call()
-			
 			can_interact = true
 
 
