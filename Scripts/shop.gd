f5808973a9932cd223a739a9832115bd0cf40228
Kevin Tu <tukevin274@gmail.com>
2025-03-15 14:04:12 -0600

    Allow spawning of UI when interacting with Shop

--- Scripts/shop.gd
+++ Scripts/shop.gd
@@ -2,12 +2,16 @@ extends StaticBody2D
 
 @onready var interactable: Area2D = $Interactable
 @onready var sprite_2d: Sprite2D = $Sprite2D
+var ui_opened := false
 
 func _ready() -> void:
 	interactable.interact = _on_interact
 
 func _on_interact():
 	print("Interacted with shop")
-	var shop_ui = preload("res://UI/shopmenu.tscn").instantiate()
-	get_tree().current_scene.add_child(shop_ui)
+	if not ui_opened:
+		ui_opened = true
+		var shop_ui = preload("res://UI/shopmenu.tscn").instantiate()
+		var canvas_layer = get_node("../CanvasLayer")
+		canvas_layer.add_child(shop_ui)
 	
