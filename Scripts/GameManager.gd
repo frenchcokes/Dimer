e4895587dc4178371e35753c3db8b0212a439383
Sean <144563579+frenchcokes@users.noreply.github.com>
2025-03-16 01:13:01 -0600

    completed

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,11 +1,12 @@
 class_name GameManager extends Node
 
-@onready var day_countdown_label: Label = get_node("Player/DayCountdown")
 @onready var night_time_spawn_point: Node2D = get_node("NightTimeSpawnPoint")
 
 var day_timer: Timer
 var day_duration: float = 10
 var game_state = 0
+
+signal timer_display(time: int)
 
 func _ready() -> void:
 	day_timer = Timer.new()
@@ -14,15 +15,20 @@ func _ready() -> void:
 	day_timer.one_shot = true
 	Globals.set_game_manager(self)
 	
-	get_node("MineStartInteract").set_callable(start_mine)
-	get_node("MineStartInteract").set_display_text("Return to the mines")
+	setup_interact_points()
 	
 	start_day_timer()
 
+func setup_interact_points():
+	var mine_start_interact = get_node("MineStartInteract")
+	mine_start_interact.set_callable(start_mine)
+	mine_start_interact.set_display_text("Return to the mines")
+
 func _process(_delta: float) -> void:
-	day_countdown_label.text = str(int(round(day_timer.time_left)))
 	if(day_timer.is_stopped()):
-		day_countdown_label.text = "Night Time"
+		emit_signal("timer_display", "Night Time")
+	else:
+		emit_signal("timer_display", "Time Left: " + str(int(round(day_timer.time_left))))
 
 func start_day_timer():
 	day_timer.start(day_duration)
--- UI/hud.gd
+++ UI/hud.gd
@@ -3,16 +3,28 @@ extends Control
 @onready var money_counter: Label = $VBoxContainer/MoneyCounter
 @onready var inventory_counter: Label = $VBoxContainer/InventoryCounter
 @onready var mining_speed: Label = $VBoxContainer/MiningSpeed
+@onready var day_countdown: Label = $VBoxContainer2/DayCountdown
+
+var connected_to_manager = false
 
 func _ready() -> void:
 	var player = get_tree().current_scene.find_child("Player", true, false)  # Assuming the Player is in a group
 	if not player:
 		print("Failed to find player.")
 	player.connect("stats_updated", Callable(self, "_on_stats_updated"))
-	
+
+func _process(_delta: float) -> void:
+	if(!connected_to_manager):
+		if(Globals.get_game_manager()):
+			Globals.get_game_manager().timer_display.connect(countdown_updated)
+			connected_to_manager = true
 
 func _on_stats_updated(money, inventoryValue, player_damage):
 	if money and inventoryValue and player_damage:
 		money_counter.text = "Money: " + str(money)
 		inventory_counter.text = "Inventory Value: " + str(inventoryValue)
 		mining_speed.text = "Mining Speed: " + str(player_damage) 
+
+func countdown_updated(display: String):
+	if(day_countdown):
+		day_countdown.text = display
