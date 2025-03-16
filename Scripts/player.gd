fb09f3c35150c631e0c22b957270d3a93c40fbcf
Kevin Tu <tukevin274@gmail.com>
2025-03-15 19:07:41 -0600

    Fix player

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+class_name Player
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 const interact_indicator_prefab: PackedScene = preload("../Prefabs/Interact_Indicator.tscn")
 
